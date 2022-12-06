fce4649208980aa085f7281f355a9adeac91836a
Arjun-Pat <92606393+Arjun-Pat@users.noreply.github.com>
2022-12-06 17:06:07 -0600

    Update OrderScreen.js
    Concurrent with demo, api key placeholder inserted

--- screens/OrderScreen.js
+++ screens/OrderScreen.js
@@ -8,6 +8,8 @@ import banana from './banana.png'
 import Icon from 'react-native-vector-icons/Ionicons';
 
 import MapView from 'react-native-maps';
+import {StyleSheet, Polyline, Dimensions} from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
 
 import { Marker } from 'react-native-maps';
 
@@ -37,8 +39,13 @@ export default function OrderScreen({navigation, route}) {
   })
   
   let [toggled, setToggled] = useState(false);
+  let [toggled2, setToggled2] = useState(false);
   let [delayToggle, setDelayToggle] = useState(false);
   let [expandMessage, setExpandMessage] = useState("View Order");
+  let [completionMessage, setCompletionMessage] = useState("Complete Order");
+  let [deliveryTime, setDeliveryTime] = useState(0);
+  let [deliveryDistance, setDeliveryDistance] = useState(0);
+
   const height = useRef(new Animated.Value(1)).current;
 
   useEffect(() => {
@@ -60,17 +67,40 @@ export default function OrderScreen({navigation, route}) {
         setDelayToggle(false)
   }, [toggled]);
 
+  const handlePress2 = () => {
+    setToggled2((prev) => !prev)
+    if (completionMessage === "Complete Order")
+        //insert call to update status
+        setCompletionMessage("Delivery Complete")
+    else
+        navigation.navigate('Home', {
+            userInfo: userInfo
+          })
+  }
+
 
   const handlePress = () => {
     setToggled((prev) => !prev)
     if (expandMessage === "View Order")
+        //insert call to update status
         setExpandMessage("Minimize Order")
     else
         setExpandMessage("View Order")
   }
 
-  const statusMessage = "Driving to Restaurant" // get from Server
+  const statusMessage = "Out for Delivery" // get from Server
   const ETA = "10:15 am" // get from Server
+  const [coordinates] = useState ([
+    {
+        latitude: 33.12783688178995,
+        longitude: -96.72796024170364,
+      },
+      {
+        latitude: 33.165510,
+        longitude: -96.743950,
+      },
+  ]);
+  //const apiKey = process.env.NODE_ENV[GOOGLE_API_KEY];
 
   return (
     <View>
@@ -79,16 +109,32 @@ export default function OrderScreen({navigation, route}) {
         
         <MapView
             initialRegion={{
-            latitude: 33.12783688178995,
-            longitude: -96.72796024170364,
-            latitudeDelta: 0.000922,
-            longitudeDelta: 0.000421,
+            latitude: coordinates[0].latitude,
+            longitude: coordinates[0].longitude,
+            latitudeDelta: 0.0522,
+            longitudeDelta: 0.0421,
             }}
             className='w-full h-[100vh] relative'
         >
-            <Marker
-                coordinate={{ latitude : 33.12783688178995 , longitude : -96.72796024170364 }}
-            />
+        <MapViewDirections
+          origin={coordinates[0]}
+          destination={coordinates[1]}
+          apikey={'API_KEY'} // insert your API Key here
+          strokeWidth={4}
+          strokeColor="#33A5FF"
+          onReady={result => {
+            //MapData.distance = result.distance
+            //MapData.duration = result.duration
+            setDeliveryDistance(result.distance * 0.621371)
+            setDeliveryTime(result.duration)
+            console.log(`Distance: ${result.distance} km`)
+            console.log(`Distance: ${result.distance * 0.621371} miles`)
+            console.log(`Duration: ${result.duration} min.`)
+            //forceUpdate()
+          }}
+        />
+        <Marker coordinate={coordinates[0]} />
+        <Marker coordinate={coordinates[1]} />
         </MapView>
 
         {/* Dev Nav */}
@@ -99,11 +145,11 @@ export default function OrderScreen({navigation, route}) {
         <View style={{position: 'absolute',  bottom: 85, left: 0, right: 0, justifyContent: 'center', alignItems: 'center'}}>
             <Animated.View 
                 id="main-bow" 
-                style={{height: height.interpolate({inputRange: [0, 1], outputRange: [300, 500]})}}
+                style={{height: height.interpolate({inputRange: [0, 1], outputRange: [400, 600]})}}
                 className="w-[85vw] bg-[#FAF9F6] mx-auto rounded-3xl border-[1px]"
             >
                 <Text className='text-3xl text-black font-bold pl-5 pt-7 mb-[-20px]'>{statusMessage}</Text>
-                <Text className='text-lg text-black font-medium pl-5 pt-7 pb-3'>Estimated Arrival Time: {ETA}</Text>
+                <Text className='text-lg text-black font-medium pl-5 pt-7 pb-3'>{parseFloat(deliveryDistance).toFixed(2)} miles, {parseFloat(deliveryTime).toFixed(2)} mins </Text>
                 {delayToggle && <Text className='text-lg text-black font-medium pl-5'>Order Details</Text>}
                 {/* Show Order if Expanded */}
                 {delayToggle && 
@@ -152,6 +198,12 @@ export default function OrderScreen({navigation, route}) {
                 <TouchableOpacity onPress={() => handlePress()} className="bg-blue-500 w-[75vw] mx-auto items-center py-5 rounded-xl mb-5">
                     <Text className="font-medium text-white text-xl">{expandMessage}</Text>
                 </TouchableOpacity>
+                {toggled2 ? <TouchableOpacity onPress={() => handlePress2()} className="bg-green-500 w-[75vw] mx-auto items-center py-5 rounded-xl mb-5">
+                    <Text className="font-medium text-white text-xl">{completionMessage}</Text>
+                </TouchableOpacity>
+                : <TouchableOpacity onPress={() => handlePress2()} className="bg-blue-500 w-[75vw] mx-auto items-center py-5 rounded-xl mb-5">
+                <Text className="font-medium text-white text-xl">{completionMessage}</Text>
+            </TouchableOpacity>}
             </Animated.View>
         </View>
     </View>
